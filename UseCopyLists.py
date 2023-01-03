65128241bbee79571673eeb36e3ac65c444dc5ac
Caro-maker <57056884+Caro-maker@users.noreply.github.com>
2023-01-02 22:02:14 -0600

    Create UseCopyLists.py

---
+++ UseCopyLists.py
@@ -0,0 +1,25 @@
+# https://www.youtube.com/shorts/tMM8jZbh-q8
+
+lista = ['_', '_', '_', '_', '_']
+
+lista2 = lista
+lista3 = lista
+lista4 = lista
+
+#dejó la carne bien tiesa
+#¿como esta la carne?
+lista2[3] = "bien"
+lista2[4] = "tiesa"
+#print(lista2)
+
+#se me quita el hambre
+#que le pasa al hambre?
+lista3[0] = "se"
+lista3[1] = "me"
+lista3[2] = "quita"
+
+#el señor pone la mesa
+#¿Cuál es el verbo de la oración?
+lista4[2] = "pone"
+
+print(lista4)
